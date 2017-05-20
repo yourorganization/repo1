119242335db8edbe1ba74ed328e5a6631b875dd9
ABC <ABC@IP500>
2017-05-21 02:11:44 +0530

    .

--- Project1/src/com/myorganization/tests/samples/sanitytests.java
+++ Project1/src/com/myorganization/tests/samples/sanitytests.java
@@ -1,16 +1,39 @@
 package com.myorganization.tests.samples;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class sanitytests {
-
+	WebDriver driver=null;
 	
 @Test
 public void TC001()
 {
-	System.out.println("TC001 Passed..");
+	System.out.println("Executing Step 1.....");
+	
+	System.setProperty("webdriver.gecko.driver", "lib\\geckodriver-v0.16.1-win64\\geckodriver.exe");
+	driver = new FirefoxDriver();
+	
+	System.out.println("Executing Step 2.....");	
+	driver.get("http://www.google.com");
+	
+	System.out.println("Executing Step 3.....");
+	WebElement label1 = driver.findElement(By.xpath(".//*[@id='hplogo']/div"));
+	System.out.println("Executing Step 4.....");
+	Assert.assertEquals( label1.getText(),"Indians");
+	
+	
 }
 	
-	
+	@AfterTest
+	public void test()
+	{
+		driver.quit();
+	}
 	
 }
