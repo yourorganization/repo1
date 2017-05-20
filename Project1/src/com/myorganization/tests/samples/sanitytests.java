92e5a6728c30b043fe958fed34010a798db4e9c5
Test <Test@IP500>
2017-05-21 01:19:04 +0530

    .

---
+++ Project1/src/com/myorganization/tests/samples/sanitytests.java
@@ -0,0 +1,16 @@
+package com.myorganization.tests.samples;
+
+import org.testng.annotations.Test;
+
+public class sanitytests {
+
+	
+@Test
+public void TC001()
+{
+	System.out.println("TC001 Passed..");
+}
+	
+	
+	
+}
